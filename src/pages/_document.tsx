8dc5e1479cb5c0fc76f1399c94fc3554f71958fb
jungsungwook <jswcyber@naver.com>
2023-03-03 15:02:27 +0900

    feat: login/register page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  console.log("# API URL ROOT ---> "+process.env.NEXT_PUBLIC_API_ROOT)
+  console.log("# API URL PORT ---> "+process.env.NEXT_PUBLIC_API_PORT)
   return (
     <Html lang="en">
       <Head />
